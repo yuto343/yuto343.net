c8ee4ada1d7948a94f1f736f6a0f862eaa2590e6
yuto343 <namawasabi1229@gmail.com>
2023-01-16 21:13:26 +0900

    astroのバージョンをbeta.2に

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,12 @@
 import { z, defineCollection } from "astro:content";
 
 const events = defineCollection({
-  schema: {
+  schema: z.object({
     title: z.string(),
     at: z.string().optional(),
     date: z.date(),
     type: z.enum(["conf", "company", "article", "development"]),
-  },
+  }),
 });
 
 export const collections = {

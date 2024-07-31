cde62562a32a7c5bc7f56cd0ed1ec98b8689b3a8
yuto343 <namawasabi1229@gmail.com>
2024-07-31 15:22:51 +0900

    mdxを入れた

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()]
 });

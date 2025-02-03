9a417a4d022db2ccdf0184142399fd9cf0de3b49
yuto343 <namawasabi1229@gmail.com>
2025-02-03 16:04:45 +0900

    fix setting

--- functions/conference/[[slug]].ts
+++ functions/conference/[[slug]].ts
@@ -2,6 +2,7 @@ export const onRequest: PagesFunction = async ({ params }) => {
   const ORIGIN = "https://topic-site-migration.conference-as2.pages.dev";
 
   const { slug } = params;
-
-  return fetch(`${ORIGIN}/${typeof slug === "string" ? slug : slug.join("/")}`);
+  return fetch(
+    `${ORIGIN}/conference/${typeof slug === "string" ? slug : slug.join("/")}`
+  );
 };

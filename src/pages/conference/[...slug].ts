1ddafab0ab5bf5725973a673d12a90940427c492
yuto343 <namawasabi1229@gmail.com>
2025-02-06 16:33:27 +0900

    test

--- src/pages/conference/[...slug].ts
+++ src/pages/conference/[...slug].ts
@@ -1,19 +1,46 @@
+export const prerender = false;
 import type { APIRoute } from "astro";
-export const GET: APIRoute = async ({ params }) => {
-  const ORIGIN = "https://topic-site-migration.conference-as2.pages.dev";
 
+const ORIGIN = "https://topic-site-migration.conference-as2.pages.dev";
+
+export const GET: APIRoute = async ({ params }) => {
   const { slug } = params;
-  const target = `${ORIGIN}/conference/${typeof slug === "string" ? slug : slug.join("/")}`;
+
+  // /conferenceに直アクセスした時
+  if (slug === undefined) {
+    return show404();
+  }
+  const target = `${ORIGIN}/conference/${slug}`;
   const response = await fetch(target);
-  console.log(target);
-  console.log(response);
-  return new Response(response.body);
+  const contentType = response.headers.get("Content-Type");
+
+  if (response.status === 404) {
+    return show404();
+  }
+  if (!response.ok) {
+    return show404();
+  }
+  if (contentType === null) {
+    return show404();
+  }
+
+  return new Response(response.body, {
+    status: response.status,
+    headers: {
+      "Content-Type": contentType,
+    },
+  });
 };
 
-// export const onRequest: PagesFunction = async ({ params }) => {
-//   const ORIGIN = "https://topic-site-migration.conference-as2.pages.dev";
+const show404 = async (): Promise<Response> => {
+  const response = await fetch(`${ORIGIN}/404`);
+  const contentType = response.headers.get("Content-Type") ?? "text/html";
+  const notFoundBody = await response.text();
 
-//   const { slug } = params;
-//   const target = `${ORIGIN}/conference/${typeof slug === "string" ? slug : slug.join("/")}`;
-//   return fetch(target);
-// };
+  return new Response(notFoundBody, {
+    status: 404,
+    headers: {
+      "Content-Type": contentType,
+    },
+  });
+};
